b06aa6f35003b54d72aea4e45e4f7c808435a09d
sifat-samin <sifatsamin19@gmail.com>
2025-05-22 21:54:28 +0600

    Navbar Completed

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,16 +1,25 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import { Navbar, Hero, About, Experience, Tech, Works, Feedbacks, Contact, StarsCanvas } from '../components';
 
 const Main = () => {
-    return (
-        <div className='relative z-0 bg-primary'>
-            <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-                <Navbar />
-                <Outlet />
-            </div>
-        </div>
+  return (
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
 
-    );
+      <div className=''>
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
 };
+
 
 export default Main;
