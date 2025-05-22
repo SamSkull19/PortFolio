e42f3b1cb4fb95da2adbd4e8b6b52b8397533b68
sifat-samin <sifatsamin19@gmail.com>
2025-05-22 20:07:23 +0600

    Components Checked

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,8 +4,8 @@ import Navbar from '../components/Navbar';
 const Main = () => {
     return (
         <div className='relative z-0 bg-primary'>
-            <Navbar />
             <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                <Navbar />
                 <Outlet />
             </div>
         </div>
